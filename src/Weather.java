31026c5fa83ae8aaa95f9b75b539ce667643ccdb
pabeler <pabeler@o2.pl>
2022-12-14 00:05:31 +0100

    Added template of GUI

---
+++ src/Weather.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class Weather extends JFrame {
+
+    private JPanel panel1;
+    private JPanel mainPanel;
+    private JLabel cityName;
+    private JTextField cityNameInput;
+    private JButton submitButton;
+}
